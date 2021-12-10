00e42e5953ce8defe06f93cbd9c0a1e072e0de7a
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2021-12-10 20:16:52 +0700

    add pegawai accounts

--- sifactory/src/main/java/apap/TugasAkhir/siFactory/security/WebSecurityConfig.java
+++ sifactory/src/main/java/apap/TugasAkhir/siFactory/security/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/**").permitAll()
                 .antMatchers("/v3/api-docs/**").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
-                // .antMatchers("/pegawai/add").hasAuthority("ADMIN")
+                .antMatchers("/pegawai/add").hasAuthority("ADMIN")
                 .antMatchers("/delivery/viewAll").hasAnyAuthority("STAFF_KURIR", "STAFF_OPERASIONAL")
                 .antMatchers("/item/update-stok/**").hasAuthority("STAFF_GUDANG")
                 .antMatchers("/item/update-stok/rui/**").hasAuthority("STAFF_GUDANG")
@@ -57,14 +57,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     // return new BCryptPasswordEncoder();
     // }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder)
-                .withUser("useradmin").password(encoder.encode("Admin123"))
-                .roles("ADMIN");
-    }
+    // @Autowired
+    // public void configureGlobal(AuthenticationManagerBuilder auth) throws
+    // Exception {
+    // BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    // auth.inMemoryAuthentication()
+    // .passwordEncoder(encoder)
+    // .withUser("useradmin").password(encoder.encode("Admin123"))
+    // .roles("ADMIN");
+    // }
 
     // @Autowired
     // public void configureGlobal(AuthenticationManagerBuilder auth) throws
@@ -74,15 +75,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     // .withUser("useradmin").password(encoder().encode("Admin!123")).roles("Admin");
     // }
 
-    // @Autowired
-    // private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-    // @Autowired
-    // public void configAuthentication(AuthenticationManagerBuilder auth) throws
-    // Exception {
-    // BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    // auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-    // }
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
 
     // @Autowired
     // private UserDetailsService userDetailsService;
